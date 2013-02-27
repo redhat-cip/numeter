f8bc8f686cb50b94e651a32194432262d9a81553
l0nkaji <alexandre@enovance.com>
2013-02-27 10:49:43 +0100

    Modify the license to AGPL, the long_description and somes others minors cosmetic changes.

--- storage/setup.py
+++ storage/setup.py
@@ -1,67 +1,66 @@
 #!/usr/bin/env python
 #
-# $Id: setup.py,v 1.32 2010-10-17 15:47:21 gaelL Exp $
+# $Id: setup.py,v 0.2.3.4 2013-02-27 09:56:56 gaelL Exp $
 #
-#    Copyright (C) 2010-2013  Gaël Lambert (gaelL) <gael@gael-lambert.org>
+# Copyright (C) 2010-2013  Gaël Lambert (gaelL) <gael@gael-lambert.org>
 #
-#    This file is part of numeter
+# This file is part of numeter
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Numeter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
 if __name__ == '__main__':
 
     setup(name='redmon-storage',
-        version='0.9.15.1',
-        description='Numeter Storage',
-        long_description="""Limited Shell (numeter) is lets you restrict the \
-environment of any user. It provides an easily configurable shell: just \
-choose a list of allowed commands for every limited account.""",
-        author='Gaël Lambert (gaelL)',
-        author_email='gael@gael-lambert.org',
-        maintainer='Gaël Lambert (gaelL)',
-        maintainer_email='gael@gael-lambert.org',
-        keywords=['limited','shell','security','python'],
-        url='https://github.com/enovance/numeter',
-        license='GPL',
-        platforms='UNIX',
-        scripts = ['storage/numeter-storage'],
-        packages = [''],
-        package_dir = {'':'storage/module'},
-        #package_data={'': ['storage/numeter_storage.py']},
-        #data_files = [('/etc', ['etc/numeter.conf']),                                                                                                                                                                                   
-        #              ('/etc/logrotate.d', ['etc/logrotate.d/numeter']),
-        #              ('share/doc/numeter',['README', 'COPYING', 'CHANGES']),
-        #              ('share/man/man1/', ['man/numeter.1']) ],
-        data_files = [('/etc/numeter', ['storage/numeter_storage.cfg','storage/collector-list']), 
-        #              ('/etc/nginx/sites-available', ['storage/storage-web/numeter-storage-web']) ,
-        #              ('/etc/uwsgi/apps-available', ['storage/storage-web/numeter-storage-uwsgi.ini']) ,
-                      ('/usr/share/numeter/storage', ['storage/storage-web/numeter_uwsgi.py']) ,
-                      ('/var/log/numeter', '') ,
-                      ('/var/lib/numeter/rrds', ''),
-                      ('/etc/cron.d', ['storage/numeter-storage-cron']) ],
-        classifiers=[
-                'Development Status :: 4 - Beta',
-                'Environment :: Console'
-                'Intended Audience :: Advanced End Users',
-                'Intended Audience :: System Administrators',
-                'License :: OSI Approved :: GNU General Public License v3',
-                'Operating System :: POSIX',
-                'Programming Language :: Python',
-                'Topic :: Security',
-                'Topic :: System Shells',
-                'Topic :: Terminals'
-                ],
-    )
+          version='0.2.3.4',
+          description='Numeter Storage',
+          long_description="""Numeter is a new graphing solution (like Cacti for \
+          example) made by some guys working at eNovance. Poller and collector are \
+          written in Python and datas are stored in a Redis DB. The webapp is written in PHP.\
+          Documentation is available here: https://numeter.readthedocs.org""",
+          author='Gaël Lambert (gaelL)',
+          author_email='gael@gael-lambert.org',
+          maintainer='Gaël Lambert (gaelL)',
+          maintainer_email='gael@gael-lambert.org',
+          keywords=['numeter','graphing','poller','collector'],
+          url='https://github.com/enovance/numeter',
+          license='GNU Affero General Public License v3',
+          scripts = ['storage/numeter-storage'],
+          packages = [''],
+          package_dir = {'':'storage/module'},
+          #package_data={'': ['storage/numeter_storage.py']},
+          #data_files = [('/etc', ['etc/numeter.conf']),                                                                                                                                                                                   
+          #              ('/etc/logrotate.d', ['etc/logrotate.d/numeter']),
+          #              ('share/doc/numeter',['README', 'COPYING', 'CHANGES']),
+          #              ('share/man/man1/', ['man/numeter.1']) ],
+          data_files = [('/etc/numeter', ['storage/numeter_storage.cfg','storage/collector-list']), 
+                        #              ('/etc/nginx/sites-available', ['storage/storage-web/numeter-storage-web']) ,
+                        #              ('/etc/uwsgi/apps-available', ['storage/storage-web/numeter-storage-uwsgi.ini']) ,
+                        ('/usr/share/numeter/storage', ['storage/storage-web/numeter_uwsgi.py']) ,
+                        ('/var/log/numeter', '') ,
+                        ('/var/lib/numeter/rrds', ''),
+                        ('/etc/cron.d', ['storage/numeter-storage-cron']) ],
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'Environment :: Console',
+              'Intended Audience :: Advanced End Users',
+              'Intended Audience :: System Administrators',
+              'License :: OSI Approved :: GNU Affero General Public License v3',
+              'Operating System :: POSIX',
+              'Programming Language :: Python',
+              'Topic :: System :: Monitoring'
+
+          ],
+         )
