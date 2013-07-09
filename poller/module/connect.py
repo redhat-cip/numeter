0c07dc7aea9adce4846ccb134cba65f69e3710b1
Martin Bureau <m.bureau@enovance.com>
2013-07-09 15:56:21 -0400

    cleanup

--- poller/module/connect.py
+++ poller/module/connect.py
@@ -5,28 +5,13 @@ import socket
 class MuninSock:
 
     def __init__(self):
-        #self._logger = logger
-        #self._logger.info("Plugin Munin start")
-        #self._configParser = configParser
         self._munin_host = "127.0.0.1"
         self._munin_port = 4949
-        self._plugins_enable = ".*"
-        #self.munin_connection = None
-
-        #if configParser: self.getParserConfig()
-        #self._logger.info("section myMuninModule : plugins_enable = "
-        #                + self._plugins_enable)
-        #self._logger.info("section myMuninModule : munin_host = "
-        #                + self._munin_host)
-        #self._logger.info("section myMuninModule : munin_port = "
-        #                + str(self._munin_port))
-
 
     def __enter__(self):
         self.munin_sock = socket.create_connection((self._munin_host
                                             , self._munin_port))
         self._s = self.munin_sock.makefile()
-        #self.hello_string = self._readline()
         self.hello_string = self._s.readline().strip()
 	return self.munin_sock
 
@@ -40,6 +25,7 @@ class MuninConnection:
         self.watchdog = 1000 # watchdog for munin socket error
 
     def _readline(self):
+        self._s = self.sock.makefile()
         return self._s.readline().strip()
 
     def _iterline(self):
@@ -58,24 +44,14 @@ class MuninConnection:
     def munin_list(self):
         # Get node name
         node = self.munin_nodes()
-        # Start munin connexion
-        #c.munin_connect()
         with MuninSock() as self.sock:
             self.sock.sendall("list %s\n" % node)
-            self._s = self.sock.makefile()
             return_list = self._readline().split(' ')
-        # Close munin connexion to avoid fucked plugin
-        #c.munin_close()
         return return_list if return_list != [''] else []
 
     def munin_nodes(self):
-        # Start munin connexion
-        #c.munin_connect()
         with MuninSock() as self.sock:
             self.sock.sendall("nodes\n")
-            self._s = self.sock.makefile()
             return_node = [ line for line in self._iterline() ]
-        # Close munin connexion to avoid fucked plugin
-        #c.munin_close()
         return return_node[0] if return_node else None
 
