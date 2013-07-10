386fee32a9704590e5a0abfc8447b1583e4c582b
Martin Bureau <m.bureau@enovance.com>
2013-07-10 10:12:08 -0400

    munin connect funct. myMuninModule -> connect.py

--- poller/module/connect.py
+++ poller/module/connect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+import re
 
 class MuninSock:
 
@@ -25,7 +26,6 @@ class MuninConnection:
         self.watchdog = 1000 # watchdog for munin socket error
 
     def _readline(self):
-        self._s = self.sock.makefile()
         return self._s.readline().strip()
 
     def _iterline(self):
@@ -41,17 +41,64 @@ class MuninConnection:
                 break
             yield line
 
+
+    def munin_fetch(self, key):
+        with MuninSock() as self.sock:
+	    self._s = self.sock.makefile()
+            self.sock.sendall("fetch %s\n" % key)
+            ret = {}
+            for line in self._iterline():
+                match = re.match("^([^\.]+)\.value", line)
+                if match is None: continue
+                key = match.group(1)
+                match = re.match("^[^ ]+\s+([0-9\.U-]+)$", line)
+                if match is not None: value = match.group(1)
+                else: value = 'U'
+                ret[key] = value
+        return ret
+
+
     def munin_list(self):
         # Get node name
         node = self.munin_nodes()
         with MuninSock() as self.sock:
+            self._s = self.sock.makefile()
             self.sock.sendall("list %s\n" % node)
             return_list = self._readline().split(' ')
         return return_list if return_list != [''] else []
 
     def munin_nodes(self):
         with MuninSock() as self.sock:
+            self._s = self.sock.makefile()
             self.sock.sendall("nodes\n")
             return_node = [ line for line in self._iterline() ]
         return return_node[0] if return_node else None
+
+
+    def munin_config(self, key):
+        with MuninSock() as self.sock:
+            self._s = self.sock.makefile()
+            self.sock.sendall("config %s\n" % key)
+            ret = {}
+            for line in self._iterline():
+                if line.startswith('graph_'):
+                    try:
+                        key, value = line.split(' ', 1)
+                        ret[key] = value
+                    except ValueError:
+                        self._logger.info("myMuninModule : skipped key " + key)
+		else:
+		    # less sure but faster
+		    #key, rest = line.split('.', 1)
+		    #prop, value = rest.split(' ', 1)
+		    match = re.match("^([^\.]+)\.([^\ ]+)\s+(.+)", line)
+		    if match is None: continue
+		    key   = match.group(1)
+		    prop  = match.group(2)
+		    value = match.group(3)
+		    if not ret.get(key):
+		        ret[key] = {}
+                    ret[key][prop] = value
+        # Close munin connexion to avoid fucked plugin
+        return ret
 
