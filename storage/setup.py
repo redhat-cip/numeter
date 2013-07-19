1458f6fee24871e139eff417255f4acbf8ae0f9f
Jean-Sebastien Frerot <jean.sebastien.frerot@enovance.com>
2013-07-19 14:26:00 +0000

    Enabled package to setup the init scripts to start at boot with default file set to not start

--- storage/setup.py
+++ storage/setup.py
@@ -31,6 +31,8 @@ class my_install(install_data):
             # Rename name.init in name
             if script.endswith(".init"):
                 shutil.move(script, script[:-5])
+            if script.endswith(".default"):
+                shutil.move(script, script[:-8])
 
 if __name__ == '__main__':
 
@@ -63,6 +65,7 @@ if __name__ == '__main__':
                         ('/usr/share/numeter/storage', ['storage/storage-web/numeter_uwsgi.py']) ,
                         ('/var/log/numeter', '') ,
                         ('/etc/init.d', ['storage/numeter-storage.init']),
+                        ('/etc/default', ['storage/numeter-storage.default']),
                         ('/var/lib/numeter/rrds', ''),
                         #('/etc/cron.d', ['storage/numeter-storage-cron'])
                         ],
