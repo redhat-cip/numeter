5dcfe501d665ada7cb7626bfae372fcaf7e0c656
ZuluPro <montheanthony@hotmail.com>
2013-08-22 00:36:30 +0200

    Factorize Wild Storage with decorator

--- web-app/src/numeter/rest/resources/wild_storage.py
+++ web-app/src/numeter/rest/resources/wild_storage.py
@@ -1,6 +1,26 @@
 from django.conf.urls import url
+from django.utils.decorators import available_attrs
 from tastypie.resources import Resource
 from core.models import Storage
+from functools import wraps
+
+
+
+
+
+def find_storage():
+    def decorator(func):
+        @wraps(func, assigned=available_attrs(func))
+        def inner(self, request, *args, **kwargs):
+            self.method_check(request, allowed=['get'])
+            self.is_authenticated(request)
+            self.throttle_check(request)
+            self.log_throttled_access(request)
+
+            self.storage = Storage.objects.which_storage(request.GET['host'])
+            return func(self, request, *args, **kwargs)
+        return inner
+    return decorator
 
 
 class Wild_Storage_Resource(Resource):
@@ -20,48 +40,29 @@ class Wild_Storage_Resource(Resource):
             url(r"^data$", self.wrap_view('data'), name="api_data"),
         ]
 
+    @find_storage()
     def hinfo(self, request, *args, **kwargs):
-        self.method_check(request, allowed=['get'])
-        self.is_authenticated(request)
-        self.throttle_check(request)
-        self.log_throttled_access(request)
-        # Get data
-        storage = Storage.objects.which_storage(request.GET['host'])
-        data = storage.get_info(request.GET['host'])
+        data = self.storage.get_info(request.GET['host'])
         return self.create_response(request, data)
 
+    @find_storage()
     def list(self, request, *args, **kwargs):
-        self.method_check(request, allowed=['get'])
-        self.is_authenticated(request)
-        self.throttle_check(request)
-        self.log_throttled_access(request)
-        # Get data
-        storage = Storage.objects.which_storage(request.GET['host'])
-        data = storage.get_plugins(request.GET['host'])
+        data = self.storage.get_plugins(request.GET['host'])
         return self.create_response(request, data)
 
+    @find_storage()
     def info(self, request, *args, **kwargs):
-        self.method_check(request, allowed=['get'])
-        self.is_authenticated(request)
-        self.throttle_check(request)
-        self.log_throttled_access(request)
-        # Get data
-        storage = Storage.objects.which_storage(request.GET['host'])
-        data = storage.get_plugin_data_sources(request.GET['host'], request.GET['plugin'])
+        data = self.storage.get_plugin_data_sources(request.GET['host'], request.GET['plugin'])
         return self.create_response(request, data)
 
+    @find_storage()
     def data(self, request, *args, **kwargs):
-        self.method_check(request, allowed=['get'])
-        self.is_authenticated(request)
-        self.throttle_check(request)
-        self.log_throttled_access(request)
         # Get data
-        storage = Storage.objects.which_storage(request.GET['host'])
         req = {
           'hostid':request.GET['host'],
           'plugin':request.GET['plugin'],
           'ds':request.GET['ds'],
           'res':request.GET['res'],
         }
-        data = storage.get_data(**req)
+        data = self.storage.get_data(**req)
         return self.create_response(request, data)
