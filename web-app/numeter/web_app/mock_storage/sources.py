9b59377347717328791517f409e700b185e7b9df
ZuluPro <montheanthony@hotmail.com>
2013-09-15 15:23:20 +0200

    Improve mock sources

--- web-app/numeter/web_app/mock_storage/sources.py
+++ web-app/numeter/web_app/mock_storage/sources.py
@@ -1,4 +1,4 @@
-from random import random, randrange, choice
+from random import random, randint, randrange, choice
 from math import sin
 
 def full_random(x):
@@ -10,24 +10,25 @@ def full_random(x):
 
 def linear(x, step=1, offset_y=None):
     """Simple increasing line."""
-    offset_y = offset_y or random() * 10
+    offset_y = offset_y or randint(-50,50)
     for i in xrange(x):
         yield i + offset_y
         i += step
 
 def linear_decrease(x, step=-1, offset_y=None):
     """Simple increasing line."""
-    offset_y = offset_y or random() * 10
+    offset_y = offset_y or randint(-50,50)
     for i in xrange(x,-x,step):
         yield i + offset_y
 
-def sinus(x, offset_x=None, offset_y=None):
+def sinus(x, offset_x=None, offset_y=None, amp=None):
     """Sinux method."""
-    offset_x = offset_x or random() * 10
-    offset_y = offset_y or random() * 100
+    offset_x = offset_x or randint(-50,50)
+    offset_y = offset_y or randint(-50,50)
+    amp = amp or randint(1,20)
 
     for i in ( float(j) for j in xrange(x)):
-        yield sin(i + offset_y) + offset_x
+        yield sin(i + offset_y) * amp + offset_x
 
 def random_func(x):
     funcs = [full_random,linear,linear_decrease,sinus]
