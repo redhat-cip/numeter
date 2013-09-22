2ebd1ba0a46c73311283de7c138a25325e0844bf
ZuluPro <montheanthony@hotmail.com>
2013-09-22 21:46:49 +0200

    Add unittest: REST storage & host

--- web-app/numeter/web_app/rest/tests/__init__.py
+++ web-app/numeter/web_app/rest/tests/__init__.py
@@ -1,6 +1,8 @@
 from wild_storage import Wild_Storage_Test
 from user import User_Test, User_Forbidden_Test
 from group import Group_Test, Group_Forbidden_Test
+from storage import Storage_Test, Storage_Forbidden_Test
+from host import Host_Test, Host_Forbidden_Test
 
 
 def suite():
@@ -9,6 +11,8 @@ def suite():
       'rest.tests.wild_storage',
       'rest.tests.user',
       'rest.tests.group',
+      'rest.tests.storage',
+      'rest.tests.host',
     )
     suite = unittest.TestSuite()
     for t in TEST_CASES:
