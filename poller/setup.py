6915239ba4941519cf6d3e2496471f82d913402b
gaelL <gael.lambert@enovance.com>
2013-10-07 16:57:43 +0200

    Change distutils.core for setuptools

--- poller/setup.py
+++ poller/setup.py
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.core import setup
+from setuptools import setup
 from distutils.command.install_data import install_data
 import shutil
 
