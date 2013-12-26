74d751a4e015360be96ea92e071f005385e72aaa
ZuluPro <montheanthony@hotmail.com>
2013-12-26 17:40:23 +0100

    Make configuration maintab with angular

--- web-app/numeter_webapp/static/js/angular/configuration.js
+++ web-app/numeter_webapp/static/js/angular/configuration.js
@@ -1,29 +1,40 @@
 /*global window, angular, console*/
 (function (angular) {
-  'use strict';
-  angular.module('numeter', ['ui.bootstrap']).
-    controller('configurationMainTabCtrl', ['$scope', '$http', function ($scope, $http) {
-      $scope.maintabs = [
-        { title:"Users", content:"", url:"/configuration/user" },
-        { title:"Storage", content:"", url:"/configuration/storage" },
-        { title:"Plugin", content:"", url:"/configuration/plugin" },
-        { title:"View", content:"", url:"/configuration/view" },
-      ];
+    'use strict';
+    angular.module('numeter', ['ui.bootstrap']).
+        directive('configuration', function () {
+            return {
+                restrict: 'A',
+                replace: false,
+                transclude: true,
+                template: '<div ng-include="getTemplateUrl()"></div>'
 
-      $scope.showMainTab = function (maintab) {
-        $.ajax({
-          url: maintab.url,
-          success: function(data){
-             console.log(data);
-             maintab.content = data;
-          }
-        });
-        // $http.get(maintab.url).
-        //   success(function (data) {
-        //       console.log(data);
-        //     maintab.content = data;
-        //   });
-      };
-    }]);
+            };
+        }).
+
+        controller('configurationMainTabCtrl', ['$scope', '$http', function ($scope, $http) {
+            $scope.maintabs = [
+                {title: "Users", content: "", url: "/configuration/user"},
+                {title: "Storage", content: "", url: "/configuration/storage"},
+                {title: "Plugin", content: "", url: "/configuration/plugin"},
+                {title: "View", content: "", url: "/configuration/view"},
+            ];
+            $scope.tabs = [
+                {title: "View", content: "", url: "/configuration/view" },
+            ];
+            $scope.maintab = $scope.maintabs[0];
+
+            $scope.showMainTab = function (maintab) {
+            $scope.maintab = maintab;
+              // $http.get(maintab.url).
+              //   success(function (data) {
+              //       console.log(data);
+              //     maintab.content = data;
+              //   });
+            };
+            $scope.getTemplateUrl = function () {
+                return $scope.maintab.url;
+            };
+        }]);
 
 }(angular));
--- web-app/numeter_webapp/static/js/angular/numeter.js
+++ web-app/numeter_webapp/static/js/angular/numeter.js
@@ -1,90 +1,90 @@
 /*global window, angular, console*/
 (function (angular) {
-  'use strict';
+    'use strict';
 
-  angular.module('numeter', ['ui.bootstrap']).
-    directive('graph', ['$http', function ($http) {
-      return{
-        scope: {
-          resolution: '=',
-          url: '=',
-        },
-        templateUrl: 'media/graph.html',
-        link: function ($scope, $element) {
-          numeter.get_graph($scope.url, $element[0], $scope.resolution);
-        },
-        controller: ['$scope', '$http', function ($scope, $http) {
-          console.log($scope.url);
-        }]
-      };
-    }]).
-    directive('menu', ['$http', function ($http) {
-      return {
-        templateUrl: 'media/menu.html',
-        link: function ($scope) {
-          $http.get('rest/hosts/').
-            success(function (data) {
-              $scope.hosts = data.results;
+    angular.module('numeter', ['ui.bootstrap']).
+        directive('graph', ['$http', function ($http) {
+            return {
+                scope: {
+                    resolution: '=',
+                    url: '=',
+                },
+                templateUrl: 'media/graph.html',
+                link: function ($scope, $element) {
+                    numeter.get_graph($scope.url, $element[0], $scope.resolution);
+                },
+                controller: ['$scope', '$http', function ($scope, $http) {
+                    console.log($scope.url);
+                }]
+            };
+        }]).
+        directive('menu', ['$http', function ($http) {
+            return {
+                templateUrl: 'media/menu.html',
+                link: function ($scope) {
+                    $http.get('rest/hosts/').
+                        success(function (data) {
+                            $scope.hosts = data.results;
+                        });
+                },
+                controller: ['$scope', '$http', function ($scope, $http) {
+                    $scope.loadCategories = function (hosts) {
+                        angular.forEach(hosts, function (host) {
+                            $http.get('hosttree/host/' + host.id).
+                                success(function (categories) {
+                                    host.categories = categories.map(function (category) {
+                                        return {name: category, plugins: [], open: false};
+                                    });
+                                });
+                        });
+                    };
+
+                    $scope.loadPlugins = function (host, chosen_category) {
+                        angular.forEach(host.categories, function (category) {
+                            if (chosen_category == category) {
+                                $http.get('hosttree/category/' + host.id, {params: {category: category.name}}).
+                                    success(function (plugins) {
+                                        category.plugins = plugins;
+                                        $scope.displayGraph(host.id, plugins, true);
+                                    });
+                            }
+                        });
+                    };
+
+                    $scope.displayGraph = function (host_id, plugins, open) {
+                        if (open) {
+                            return;
+                        }
+                        $scope.$emit('displayGraph', host_id, plugins);
+                    };
+
+                }]
+            };
+        }]).
+        controller('resolutionCtrl', ['$scope', function ($scope) {
+            $scope.select = function (value) {
+                $scope.$emit('resChange', value);
+            };
+        }]).
+        controller('graphCtrl', ['$scope', function ($scope) {
+            $scope.selected = 'Daily';
+            $scope.$on('resChange', function (event, resolution) {
+                $scope.selected = resolution;
             });
-        },
-        controller: ['$scope', '$http', function ($scope, $http) {
-          $scope.loadCategories = function (hosts) {
-            angular.forEach(hosts, function (host) {
-              $http.get('hosttree/host/' + host.id).
-                success(function (categories) {
-                  host.categories = categories.map(function (category) {
-                    return {name: category, plugins: [], open: false};
-                  });
-                });
-            });
-          };
 
-          $scope.loadPlugins = function (host, chosen_category) {
-            angular.forEach(host.categories, function (category) {
-                  if ( chosen_category == category ) {
-              $http.get('hosttree/category/' + host.id, {params: {category: category.name}}).
-                success(function (plugins) {
-                  category.plugins = plugins;
-                    $scope.displayGraph(host.id, plugins, true);
-                });
-                  }
-            });
-          };
-
-          $scope.displayGraph = function (host_id, plugins, open) {
-            if (open) {
-              return;
-            }
-            $scope.$emit('displayGraph', host_id, plugins);
-          };
-
-        }]
-      };
-    }]).
-    controller('resolutionCtrl', ['$scope', function ($scope) {
-      $scope.select = function (value) {
-        $scope.$emit('resChange', value);
-      };
-    }]).
-    controller('graphCtrl', ['$scope', function ($scope) {
-      $scope.selected = 'Daily';
-      $scope.$on('resChange', function (event, resolution) {
-        $scope.selected = resolution;
-      });
-
-      $scope.$on('displayGraph', function (event, host_id, plugins) {
-        $scope.graphs = [];
-        plugins.map(function (plugin) {          
-          this.push({url: "get/graph/" + host_id + "/" + plugin.plugin, resolution: $scope.selected });
-        }, $scope.graphs);
-      });
-      $scope.$on('resChange', function (event) {
-        var old_graphs = $scope.graphs;
-        $scope.graphs = []
-        old_graphs.map(function (graph) {          
-          this.push({url: graph.url, resolution: $scope.selected });
-        }, $scope.graphs);
-      });
-    }]).
+            $scope.$on('displayGraph', function (event, host_id, plugins) {
+                $scope.graphs = [];
+                plugins.map(function (plugin) {          
+                  this.push({url: "get/graph/" + host_id + "/" + plugin.plugin, resolution: $scope.selected });
+                }, $scope.graphs);
+             });
+             $scope.$on('resChange', function (event) {
+                var old_graphs = $scope.graphs;
+                $scope.graphs = [];
+                old_graphs.map(function (graph) {
+                    this.push({url: graph.url, resolution: $scope.selected });
+                }, $scope.graphs);
+             });
+      }]);
 
 }(angular));
