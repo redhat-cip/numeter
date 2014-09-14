66d150df73dd29213acdea6f67270b0e51c2cdba
Gael Lambert <gael.lambert@enovance.com>
2014-09-14 23:50:58 +0200

    [docs] add setup.py for readthedocs and virtualenv for api.rst page

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 
-# Dirty hack for readthedoc and virtualenv (needed for api doc)
+# Dirty hack to build numeter api doc with virtualenv (for readthedoc)
+# You should not use this script. It's for readthedocs only while all python
+# modules are not merged into one numeter directory
 
 import subprocess
+import sys
+from os.path import dirname, join
+import os
 
-subprocess.call("cd common && python setup.py install", shell=True)
-subprocess.call("cd storage && python setup.py install", shell=True)
-subprocess.call("cd poller && python setup.py install", shell=True)
+on_readthedocs = os.environ.get('READTHEDOCS', None) == 'True'
+
+if 'install' in sys.argv and on_readthedocs:
+
+    for package in ['common', 'storage', 'poller']:
+        pip = join(dirname(sys.executable), 'pip')
+        subprocess.call("%s install -U %s" % (pip, package), shell=True)
+        subprocess.call("%s install -e %s" % (pip, package), shell=True)
