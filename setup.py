664704313574ccddadac2010725e0182b8deee33
Gael Lambert <gael.lambert@enovance.com>
2014-09-14 23:39:49 +0200

    [doc] try dirty hack for readthedoc

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-#!/bin/bash
 
 # Dirty hack for readthedoc and virtualenv (needed for api doc)
 
-cd common && python setup.py install
-cd -
-cd storage && python setup.py install
-cd -
-cd poller && python setup.py install
-cd -
+import subprocess
+
+subprocess.call("cd common && python setup.py install", shell=True)
+subprocess.call("cd storage && python setup.py install", shell=True)
+subprocess.call("cd poller && python setup.py install", shell=True)
